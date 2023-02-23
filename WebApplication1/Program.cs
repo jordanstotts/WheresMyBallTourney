74bd43a7dd63bbb79dc0f7da8f10f69d278b7a7a
jordanstotts <jordanstotts22@gmail.com>
2023-02-23 15:35:06 -0600

    added player info to player bios page

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -1,3 +1,5 @@
+using WebApplication1.Pages;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -23,3 +25,7 @@ app.UseAuthorization();
 app.MapRazorPages();
 
 app.Run();
+
+/*var jordanStotts = new PlayerBiosModel("Jordan Stoots");
+
+PlayerBiosModel player = new PlayerBiosModel("John Smith", "@johnsmith", "johnsmith_ig", "johnsmith_fb", 25, "Software Engineer", "University of XYZ", 3, 5.2, 7);*/
