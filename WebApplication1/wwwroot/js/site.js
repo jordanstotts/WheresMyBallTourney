70802f836aa9daa11f61eb214be4e48067882417
jordanstotts <jordanstotts22@gmail.com>
2023-03-06 11:14:00 -0600

    added start of html content and css to flag recipients page

--- WebApplication1/wwwroot/js/site.js
+++ WebApplication1/wwwroot/js/site.js
@@ -2,3 +2,7 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+document.getElementById('button').onclick = function () {
+    alert("button was clicked");
+};
